9cb374f4493439479c8711e4906f22483ca63219
Murillo <murillo_eum@hotmail.com>
2017-08-25 11:18:25 -0300

    tela + inicial(), cadastrarMovimentacao(), getFloat(), getTipo e getCategoria()

--- src/projeto/visao/Tela.java
+++ src/projeto/visao/Tela.java
@@ -5,6 +5,9 @@ import java.time.Month;
 import java.util.Scanner;
 import projeto.modelo.Conta;
 import projeto.modelo.Contas;
+import projeto.modelo.Movimentacao;
+import projeto.modelo.Movimentacao.Categoria;
+import projeto.modelo.Movimentacao.Tipo;
 
 public class Tela {
     
@@ -19,6 +22,48 @@ public class Tela {
         }
         return scanner.nextInt();
     }
+    private float scFloat(){
+        while (!scanner.hasNextFloat()){
+            System.out.println("Por favor, Digite um numero valido.");
+            scanner.next();
+        }
+        return scanner.nextFloat();
+    }
+    private Tipo getTipo(){
+        System.out.println("Tipo:");
+        System.out.println("Digite:\n1 - para entrada.\n2 - para saida.");
+        resposta = scanner.next();
+        while(!resposta.equals("1")&&!resposta.equals("2")){
+            System.out.println("Valor invalido.");
+            System.out.println("Digite:\n1 - para Entrada.\n2 - para Saida.");
+            resposta = scanner.next();
+        }
+        if(resposta.equals("1")){
+            return Tipo.ENTRADA;
+        }else{
+            return Tipo.SAIDA;
+        }
+    }
+    private Categoria getCategoria(){
+        System.out.println("Categoria:");
+        System.out.println("Digite:\n1 - para Alimentacao.\n2 - para Cartao de credito.\n3 - para Dispesas domesticas.\n4 - para Saude.\n5 - para Pessoal.\n6 - para Outros.");
+        resposta = scanner.next();
+        while(!resposta.equals("1")&&!resposta.equals("2")&&!resposta.equals("3")&&!resposta.equals("4")&&!resposta.equals("5")&&!resposta.equals("6")){
+            System.out.println("Valor invalido.");
+            System.out.println("Digite:\n1 - para Alimentacao.\n2 - para Cartao de credito.\n3 - para Dispesas domesticas.\n4 - para Saude.\n5 - para Pessoal.\n6 - para Outros.");
+            resposta = scanner.next();
+        }
+        switch(resposta){
+                case "1": return Categoria.ALIMENTACAO;
+                case "2": return Categoria.CARTAO_CREDITO;
+                case "3": return Categoria.DISPESAS_DOMESTICAS;
+                case "4": return Categoria.SAUDE;
+                case "5": return Categoria.PESSOAL;
+                default : return Categoria.OUTROS;
+        }      
+    }
+    
+        
     private boolean tentarDenovo(){
         System.out.println("Deseja tentar novamente? S / N");
         resposta = scanner.next();
@@ -117,34 +162,10 @@ public class Tela {
         String nome = scanner.next();
         System.out.print("Sobrenome: ");
         String sobrenome = scanner.next();
-        int dia;
-        int mes;
-        int ano;
-        boolean erro;
         System.out.println("Nascimento:");
-        do{ 
-            erro = false;
-            System.out.print("Dia: ");
-            dia = scInt();
-            System.out.print("Mes: ");
-            mes = scInt();
-            System.out.print("Ano: ");
-            ano = scInt();
-            try{
-                LocalDate nascimento = LocalDate.of(ano, mes, dia);
-            }catch(Exception e){
-                System.out.println("Data invalida. :(");
-                System.out.println("Por favor, digite novamente a data de nascimento:");
-                erro = true;
-            }
-            if(!erro && LocalDate.of(ano, mes, dia).isAfter(LocalDate.now())){
-                System.out.println("Data invalida. :(");
-                System.out.println("Por favor, digite novamente a data de nascimento:");
-                erro = true;                
-            }
-        }while(erro);
-        LocalDate nascimento = LocalDate.of(ano, mes, dia);
+        LocalDate nascimento = getData();
         char sexo = 0;
+        boolean erro;
         do{            
             erro = false;
             System.out.print("Sexo(Digite M para MASCULINO ou F para FEMININO): ");
@@ -191,5 +212,45 @@ public class Tela {
             System.out.print("Senha: ");
             senha = scanner.next();            
         }
-    }    
+        inicial(contas.getConta(email, senha));
+    }
+    
+    public void inicial(Conta c){
+        System.out.println("---Tela Inicial---");
+        System.out.println("Digite:");
+        System.out.println("1 - para cadastrar uma movimentacao.");
+        System.out.println("2 - para gerenciar finanças.");
+        System.out.println("3 - para gerenciar perfil.");
+        System.out.println("4 - para sair");
+        resposta = scanner.next();
+        while(!resposta.equals("1")&&!resposta.equals("2")&&!resposta.equals("3")&&!resposta.equals("4")){
+            System.out.println("Resposta invalida. Por favor digite uma das opções:");
+            System.out.println("1 - para cadastrar uma movimentacao.");
+            System.out.println("2 - para gerenciar finanças.");
+            System.out.println("3 - para gerenciar perfil.");
+            System.out.println("4 - para sair");
+            resposta = scanner.next();
+        }
+        switch(resposta){
+            case "1": cadastrarMovimentacao(c);
+        }
+    }
+
+    public void cadastrarMovimentacao(Conta c){
+        System.out.println("---Cadastrar Movimentacao---");
+        System.out.println("Descricao: ");
+        String desc = scanner.next();
+        System.out.println("Data: ");
+        LocalDate data = getData();
+        System.out.println("Valor: ");
+        float valor = scFloat();
+        Tipo tipo = getTipo();
+        Categoria categoria = getCategoria();
+        
+        Movimentacao m = new Movimentacao(desc,data,valor,tipo,categoria);
+        System.out.println("Nova Movimentacao:");
+        System.out.println(m);
+        c.getMovs().addMov(m);
+        
+    }
 }
