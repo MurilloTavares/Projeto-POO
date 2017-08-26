aa2569852661a24327b5a0476cd0c592948a6055
Murillo <murillo_eum@hotmail.com>
2017-08-26 19:23:35 -0300

    Tela + gerenciarFinancas(), visualisarMovs() e gerenciarPerfil()

--- src/projeto/visao/Tela.java
+++ src/projeto/visao/Tela.java
@@ -8,12 +8,12 @@ import projeto.modelo.Contas;
 import projeto.modelo.Movimentacao;
 import projeto.modelo.Movimentacao.Categoria;
 import projeto.modelo.Movimentacao.Tipo;
+import projeto.modelo.Movimentacoes;
 
 public class Tela {
     
     Contas contas = new Contas();
     Scanner scanner = new Scanner(System.in);
-    String resposta;
     
     private int scInt(){
         while (!scanner.hasNextInt()){
@@ -32,7 +32,7 @@ public class Tela {
     private Tipo getTipo(){
         System.out.println("Tipo:");
         System.out.println("Digite:\n1 - para entrada.\n2 - para saida.");
-        resposta = scanner.next();
+        String resposta = scanner.next();
         while(!resposta.equals("1")&&!resposta.equals("2")){
             System.out.println("Valor invalido.");
             System.out.println("Digite:\n1 - para Entrada.\n2 - para Saida.");
@@ -47,7 +47,7 @@ public class Tela {
     private Categoria getCategoria(){
         System.out.println("Categoria:");
         System.out.println("Digite:\n1 - para Alimentacao.\n2 - para Cartao de credito.\n3 - para Dispesas domesticas.\n4 - para Saude.\n5 - para Pessoal.\n6 - para Outros.");
-        resposta = scanner.next();
+        String resposta = scanner.next();
         while(!resposta.equals("1")&&!resposta.equals("2")&&!resposta.equals("3")&&!resposta.equals("4")&&!resposta.equals("5")&&!resposta.equals("6")){
             System.out.println("Valor invalido.");
             System.out.println("Digite:\n1 - para Alimentacao.\n2 - para Cartao de credito.\n3 - para Dispesas domesticas.\n4 - para Saude.\n5 - para Pessoal.\n6 - para Outros.");
@@ -66,7 +66,7 @@ public class Tela {
         
     private boolean tentarDenovo(){
         System.out.println("Deseja tentar novamente? S / N");
-        resposta = scanner.next();
+        String resposta = scanner.next();
         while(!resposta.equalsIgnoreCase("S") && !resposta.equalsIgnoreCase("N")){
             System.out.println("Resposta invalida. Por favor digite 'S' para sim e 'N' para nao.");
             resposta = scanner.next();
@@ -114,7 +114,8 @@ public class Tela {
         while(true){
         System.out.println("---TELA DE LOGIN---");
         System.out.println("Digite:\n1 para logar.\n2 para criar uma conta.");
-        System.out.print("Resposta: ");resposta = scanner.next();        
+        System.out.print("Resposta: ");
+        String resposta = scanner.next();        
         while(!resposta.equals("1") && !resposta.equals("2")){
             System.out.println("Valor invalido. :(");
             System.out.print("Por favor, digite novamente sua resposta: ");
@@ -173,7 +174,7 @@ public class Tela {
         do{            
             erro = false;
             System.out.print("Sexo(Digite M para MASCULINO ou F para FEMININO): ");
-            resposta = scanner.next();
+            String resposta = scanner.next();
             switch (resposta) {
                 case "M":
                 case "m":
@@ -220,13 +221,14 @@ public class Tela {
     }
     
     public void inicial(Conta c){
+        while(true){
         System.out.println("---Tela Inicial---");
         System.out.println("Digite:");
         System.out.println("1 - para cadastrar uma movimentacao.");
         System.out.println("2 - para gerenciar finanças.");
         System.out.println("3 - para gerenciar perfil.");
         System.out.println("4 - para sair");
-        resposta = scanner.next();
+        String resposta = scanner.next();
         while(!resposta.equals("1")&&!resposta.equals("2")&&!resposta.equals("3")&&!resposta.equals("4")){
             System.out.println("Resposta invalida. Por favor digite uma das opções:");
             System.out.println("1 - para cadastrar uma movimentacao.");
@@ -236,7 +238,18 @@ public class Tela {
             resposta = scanner.next();
         }
         switch(resposta){
-            case "1": cadastrarMovimentacao(c);
+            case "1":
+                cadastrarMovimentacao(c);
+                break;
+            case "2":
+                gerenciarFinancas(c.getMovs());
+                break;
+            case "3":
+                gerenciarPerfil(c);
+                break;
+            case "4":
+                return ;
+        }
         }
     }
 
@@ -256,5 +269,149 @@ public class Tela {
         System.out.println(m);
         c.getMovs().addMov(m);
         
+    }
+    
+    public void gerenciarFinancas(Movimentacoes movs){
+        while(true){
+        System.out.println("---GERENCIAR FINANCAS---");
+        System.out.println("Digite:");
+        System.out.println("1 - para visualizar movimentacoes.");
+        System.out.println("2 - para excluir uma movimentacao.");
+        System.out.println("3 - para atualizar uma movimentacao.");
+        System.out.println("4 - para voltar.");
+        String resposta = scanner.next();
+        while(!resposta.equals("1")&&!resposta.equals("2")&&!resposta.equals("3")&&!resposta.equals("4")){
+            System.out.println("Resposta invalida. Por favor digite uma das opções:");
+            System.out.println("1 - para visualizar movimentacoes.");
+            System.out.println("2 - para excluir uma movimentacao.");
+            System.out.println("3 - para atualizar uma movimentacao.");
+            System.out.println("4 - para voltar.");
+            resposta = scanner.next();
+        }
+        switch(resposta){
+            case "1":
+                visualizarMovs(movs);
+                break;
+            case "2":
+                excluirMov(movs);
+                break;
+            case "3":
+                atualizarMov(movs);
+                break;
+            case "4":
+                return;
+        }
+        }        
+    }
+    
+    public void visualizarMovs(Movimentacoes movs){
+        System.out.println("---Movimentacoes---");
+        System.out.println(movs.getMovs());
+    }
+    public void excluirMov(Movimentacoes movs){
+        
+    }
+    public void atualizarMov(Movimentacoes movs){
+        
+    }
+    
+    public void gerenciarPerfil(Conta c){
+        while(true){
+        System.out.println("---GERENCIAR PERFIL---");
+        System.out.println("Qual campo deseja modificar?");
+        System.out.println("1 - Nome.");
+        System.out.println("2 - Sobrenome.");
+        System.out.println("3 - Nascimento.");
+        System.out.println("4 - sexo.");
+        System.out.println("5 - Para modificar a senha.");
+        System.out.println("6 - voltar.");
+        String resposta = scanner.next();
+        while(!resposta.equals("1")&&!resposta.equals("2")&&!resposta.equals("3")&&!resposta.equals("4")&&!resposta.equals("5")){
+            System.out.println("Resposta invalida. Por favor digite uma das opções:");
+            System.out.println("1 - Para modificar o nome.");
+            System.out.println("2 - Para modificar o sobrenome.");
+            System.out.println("3 - Para modificar o nascimento.");
+            System.out.println("4 - Para modificar o sexo.");
+            System.out.println("5 - Para modificar a senha.");
+            System.out.println("6 - voltar.");
+            resposta = scanner.next();
+        }
+        switch(resposta){
+            case "1":
+                System.out.print("Digite o novo nome: ");
+                String nome = scanner.next();
+                c.setNome(nome);
+                System.out.println("Seu nome agora é "+c.getNome()+".");
+                break;
+            case "2":
+                System.out.print("Digite o novo sobrenome: ");
+                String sobrenome = scanner.next();
+                c.setNome(sobrenome);
+                System.out.println("Seu sobrenome agora é "+c.getSobrenome()+".");
+                break;
+            case "3":
+                System.out.println("Digite a nova data de nascimento: ");
+                LocalDate nascimento = getData();
+                c.setNascimento(nascimento);
+                System.out.println("Sua data de nascimento agora é "+c.getNascimento()+".");
+                break;
+            case "4":
+                boolean erro;
+                char sexo;
+                do{            
+                    erro = false;
+                System.out.print("Digite o novo sexo(Digite M para MASCULINO ou F para FEMININO): ");
+                String resp = scanner.next();
+                    switch (resp) {
+                        case "M":
+                        case "m":
+                            sexo = 'M';
+                            break;
+                        case "F":
+                        case "f":
+                            sexo = 'F';
+                            break;
+                    default:
+                        System.out.println("Valor invalido. :(");
+                        System.out.println("Por favor, digite novamente seu sexo.");
+                        erro = true;
+                        break;
+                    }
+                }while(erro);                
+                break;
+            case "5":
+                System.out.println("Digite sua senha antiga: ");
+                String senha = scanner.next();
+                while(!c.getSenha().equals(senha)){
+                    System.out.println("Senha incorreta.");
+                    if(!tentarDenovo()){
+                    return;
+                    }else{
+                        System.out.println("Digite sua senha antiga: ");
+                        senha = scanner.next();
+                    }
+                }
+                    System.out.println("Digite sua nova senha: ");
+                    String novasenha = scanner.next();
+                    System.out.println("Confirme sua nova senha: ");
+                    String confnovasenha = scanner.next();
+                    while(!novasenha.equals(confnovasenha)){
+                        System.out.println("Erro. As senhas não são iguais.");
+                        if(!tentarDenovo()){
+                            return;
+                        }else{
+                            System.out.println("Digite sua nova senha: ");
+                            novasenha = scanner.next();
+                            System.out.println("Confirme sua nova senha: ");
+                            confnovasenha = scanner.next();
+                        }
+                    }
+                
+                break;
+            case "6":
+                return;
+        }
+        }    
+               
     }
 }
